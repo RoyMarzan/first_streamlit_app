aa1190d7849b4e2d9c217ce6f638641a04a752d9
RoyMarzan <146403665+RoyMarzan@users.noreply.github.com>
2023-10-03 14:38:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("My Fruit Load List:")
 streamlit.text(my_data_rows)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
